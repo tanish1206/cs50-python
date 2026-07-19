b6d52e831c500f7038e7fd0620f5ea2eaa6f9bed
tanish1206 <tanishsoni1206@gmail.com>
2026-07-19 20:45:13 +0530

    Update pattern17.py

--- DSA/Pattern/pattern17.py
+++ DSA/Pattern/pattern17.py
@@ -8,16 +8,22 @@ class Solution:
             #for printing spaces##for inner loops columns
             for j in range(N-1-i):
                 print(" ", end="")
-            #for printing stars
-            for j in range(2*i+1):
+
+            #for increasing letters
+            for j in range(i+1):
                 print(chr(65+j), end="")  
+
+            #for decreasing letters
+            for j in range(i-1,-1,-1):
+                print(chr(65+j), end="")
+
             #for printing spaces 
             for j in range(N-1-i):
                 print(" ", end="")
 
             print()
 sol=Solution()
-N=5
+N=4
 sol.pattern17(N)
 
 
